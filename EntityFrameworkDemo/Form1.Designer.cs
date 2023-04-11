9c718d36ad0f8ebd24467742cfb510721276b612
Kaan Tosun <tr.kaantsn@gmail.com>
2023-04-11 18:21:18 +0300

    Add project files.

---
+++ EntityFrameworkDemo/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace EntityFrameworkDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxAddStock = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxAddPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxAddName = new System.Windows.Forms.TextBox();
+            this.dgwProducts = new System.Windows.Forms.DataGridView();
+            this.gbxUptade = new System.Windows.Forms.GroupBox();
+            this.btnUptade = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxUptadeStock = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxUptadePrice = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbxUptadeName = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).BeginInit();
+            this.gbxUptade.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbxAddStock);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbxAddPrice);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbxAddName);
+            this.groupBox1.Location = new System.Drawing.Point(22, 321);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 210);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add a product";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(182, 134);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(108, 35);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(53, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // tbxAddStock
+            // 
+            this.tbxAddStock.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxAddStock.Location = new System.Drawing.Point(112, 100);
+            this.tbxAddStock.Name = "tbxAddStock";
+            this.tbxAddStock.Size = new System.Drawing.Size(178, 28);
+            this.tbxAddStock.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(53, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Stock";
+            // 
+            // tbxAddPrice
+            // 
+            this.tbxAddPrice.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxAddPrice.Location = new System.Drawing.Point(112, 66);
+            this.tbxAddPrice.Name = "tbxAddPrice";
+            this.tbxAddPrice.Size = new System.Drawing.Size(178, 28);
+            this.tbxAddPrice.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(25, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Unit Price";
+            // 
+            // tbxAddName
+            // 
+            this.tbxAddName.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxAddName.Location = new System.Drawing.Point(112, 32);
+            this.tbxAddName.Name = "tbxAddName";
+            this.tbxAddName.Size = new System.Drawing.Size(178, 28);
+            this.tbxAddName.TabIndex = 1;
+            // 
+            // dgwProducts
+            // 
+            this.dgwProducts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwProducts.Location = new System.Drawing.Point(22, 12);
+            this.dgwProducts.Name = "dgwProducts";
+            this.dgwProducts.RowHeadersWidth = 51;
+            this.dgwProducts.RowTemplate.Height = 24;
+            this.dgwProducts.Size = new System.Drawing.Size(866, 275);
+            this.dgwProducts.TabIndex = 1;
+            this.dgwProducts.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwProducts_CellClick);
+            // 
+            // gbxUptade
+            // 
+            this.gbxUptade.Controls.Add(this.btnUptade);
+            this.gbxUptade.Controls.Add(this.label4);
+            this.gbxUptade.Controls.Add(this.tbxUptadeStock);
+            this.gbxUptade.Controls.Add(this.label5);
+            this.gbxUptade.Controls.Add(this.tbxUptadePrice);
+            this.gbxUptade.Controls.Add(this.label6);
+            this.gbxUptade.Controls.Add(this.tbxUptadeName);
+            this.gbxUptade.Location = new System.Drawing.Point(570, 321);
+            this.gbxUptade.Name = "gbxUptade";
+            this.gbxUptade.Size = new System.Drawing.Size(318, 210);
+            this.gbxUptade.TabIndex = 8;
+            this.gbxUptade.TabStop = false;
+            this.gbxUptade.Text = "Uptade a product";
+            // 
+            // btnUptade
+            // 
+            this.btnUptade.Location = new System.Drawing.Point(182, 134);
+            this.btnUptade.Name = "btnUptade";
+            this.btnUptade.Size = new System.Drawing.Size(108, 35);
+            this.btnUptade.TabIndex = 7;
+            this.btnUptade.Text = "Uptade";
+            this.btnUptade.UseVisualStyleBackColor = true;
+            this.btnUptade.Click += new System.EventHandler(this.btnUptade_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(53, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Name";
+            // 
+            // tbxUptadeStock
+            // 
+            this.tbxUptadeStock.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxUptadeStock.Location = new System.Drawing.Point(112, 100);
+            this.tbxUptadeStock.Name = "tbxUptadeStock";
+            this.tbxUptadeStock.Size = new System.Drawing.Size(178, 28);
+            this.tbxUptadeStock.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(53, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Stock";
+            // 
+            // tbxUptadePrice
+            // 
+            this.tbxUptadePrice.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxUptadePrice.Location = new System.Drawing.Point(112, 66);
+            this.tbxUptadePrice.Name = "tbxUptadePrice";
+            this.tbxUptadePrice.Size = new System.Drawing.Size(178, 28);
+            this.tbxUptadePrice.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(25, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 21);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Unit Price";
+            // 
+            // tbxUptadeName
+            // 
+            this.tbxUptadeName.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbxUptadeName.Location = new System.Drawing.Point(112, 32);
+            this.tbxUptadeName.Name = "tbxUptadeName";
+            this.tbxUptadeName.Size = new System.Drawing.Size(178, 28);
+            this.tbxUptadeName.TabIndex = 1;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(456, 496);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(108, 35);
+            this.btnRemove.TabIndex = 8;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 583);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.gbxUptade);
+            this.Controls.Add(this.dgwProducts);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).EndInit();
+            this.gbxUptade.ResumeLayout(false);
+            this.gbxUptade.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgwProducts;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxAddStock;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxAddPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxAddName;
+        private System.Windows.Forms.GroupBox gbxUptade;
+        private System.Windows.Forms.Button btnUptade;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxUptadeStock;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxUptadePrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbxUptadeName;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ EntityFrameworkDemo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ProductDal _productDal = new ProductDal();
+        private void LoadProducts()
+        {
+            dgwProducts.DataSource = _productDal.GetAll();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadProducts();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+                Name = tbxAddName.Text,
+                UnitPrice = Convert.ToDecimal(tbxAddPrice.Text),
+                StockAmount = Convert.ToInt32(tbxAddStock.Text)
+
+            });
+            LoadProducts();
+            MessageBox.Show("Added!");
+        }
+
+        private void dgwProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxUptadeName.Text = dgwProducts.CurrentRow.Cells[1].Value.ToString(); 
+            tbxUptadePrice.Text = dgwProducts.CurrentRow.Cells[2].Value.ToString();
+            tbxUptadeStock.Text = dgwProducts.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnUptade_Click(object sender, EventArgs e)
+        {
+            _productDal.Update(new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value),
+                Name = tbxUptadeName.Text,
+                UnitPrice = Convert.ToDecimal(tbxUptadePrice.Text),
+                StockAmount = Convert.ToInt32(tbxUptadeStock.Text)
+
+            });
+            LoadProducts();
+            MessageBox.Show("Uptaded!");
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            _productDal.Delete(new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value)
+            });
+            LoadProducts();
+            MessageBox.Show("Deleted!");
+        }
+    }
+}
